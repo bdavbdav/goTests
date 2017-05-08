2e3145e65b11b8f4b9a0adae5377680b63aea893
ben <bdavjones@gmail.com>
2017-05-08 16:14:44 +0100

    Initial Commit

--- hello.go
+++ hello.go
@@ -6,5 +6,5 @@ import (
 )
 
 func main() {
-	fmt.Println("What is this bullshit")
+	fmt.Println("Hello World!! ")
 }
--- helloWorld/main.go
+++ helloWorld/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("BOLLOCKS!!")
+	fmt.Println("Hello World?!!")
 }
